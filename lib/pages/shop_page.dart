5a735e2c9cbd3ae7745c285e2a1ea5c9134a08c7
Dirk Ulbricht <dirk.ulbricht@gmail.com>
2024-08-29 14:18:56 +0200

    Added search functionality to ShopPage with TextEditingController and search results list

--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -12,28 +12,60 @@ class ShopPage extends StatefulWidget {
 }
 
 class _ShopPageState extends State<ShopPage> {
+  final TextEditingController _searchController = TextEditingController();
+  List<Shoe> _searchResults = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _searchController.addListener(_searchShoes);
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  void _searchShoes() {
+    final cart = Provider.of<Cart>(context, listen: false);
+    final query = _searchController.text.toLowerCase();
+    _searchResults = cart.shoeShop.where((shoe) {
+      return shoe.name.toLowerCase().contains(query) ||
+          shoe.description.toLowerCase().contains(query);
+    }).toList();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(children: [
       Padding(
         padding: const EdgeInsets.all(20.0),
         child: Container(
-            padding: EdgeInsets.all(12.0),
-            margin: const EdgeInsets.symmetric(horizontal: 20.0),
-            decoration: BoxDecoration(
-              color: Colors.grey[200],
-              borderRadius: BorderRadius.circular(15.0),
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "Search",
-                  style: TextStyle(color: Colors.grey),
+          padding: EdgeInsets.all(12.0),
+          margin: const EdgeInsets.symmetric(horizontal: 20.0),
+          decoration: BoxDecoration(
+            color: Colors.grey[200],
+            borderRadius: BorderRadius.circular(15.0),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _searchController,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Search',
+                    hintStyle: TextStyle(color: Colors.grey),
+                  ),
                 ),
-                Icon(Icons.search)
-              ],
-            )),
+              ),
+              Icon(Icons.search)
+            ],
+          ),
+        ),
       ),
       Padding(
         padding: const EdgeInsets.all(1.0),
@@ -58,16 +90,18 @@ class _ShopPageState extends State<ShopPage> {
       ),
       SizedBox(height: 20.0),
       Expanded(
-          child: Consumer<Cart>(
-        builder: (context, cart, child) => ListView.builder(
-            itemCount: 4,
-            scrollDirection: Axis.horizontal,
-            itemBuilder: (context, index) {
-              return ShoeTile(
-                shoe: cart.shoeShop[index],
-              );
-            }),
-      ))
+        child: ListView.builder(
+          itemCount: _searchResults.isEmpty ? 4 : _searchResults.length,
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            return ShoeTile(
+              shoe: _searchResults.isEmpty
+                  ? Provider.of<Cart>(context, listen: false).shoeShop[index]
+                  : _searchResults[index],
+            );
+          },
+        ),
+      )
     ]);
   }
 }
